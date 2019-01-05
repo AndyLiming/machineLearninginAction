0aa04765075f68a7be71c24efe6385ea6bcf4495
AndyLiming <mingli6250@163.com>
2019-01-05 19:37:57 +0800

    update ch05 logistic regression

--- logRegress/logRegress.py
+++ logRegress/logRegress.py
@@ -1,5 +1,7 @@
 import numpy as np
 import math
+import matplotlib.pyplot as plt
+import random
 
 def loadDataset(filename):
   dataMat = []
@@ -11,8 +13,14 @@ def loadDataset(filename):
       labelMat.append(int(lineArr[2]))
   return dataMat, labelMat
 
-def sigmoid(inX):
+def sigmoid0(inX):
   return 1.0 / (1 + np.exp(-inX))
+
+def sigmoid(inX):
+  if inX >= 0:
+    return 1.0 / (1 + np.exp(-inX))
+  else:
+    return np.exp(inX) / (1 + np.exp(inX))
 
 def gradAscent(dataMatIn, labelMatIn):
   dataMatrix = np.mat(dataMatIn)
@@ -23,12 +31,56 @@ def gradAscent(dataMatIn, labelMatIn):
   maxCycle = 500  #iteration times
   weights = np.ones((n, 1))  #init weights
   for k in range(maxCycle):
-    h = sigmoid(dataMatrix * weights)
+    h = sigmoid0(dataMatrix * weights)
     error = labelMatrix - h
     weights = weights + alpha * dataMatrix.transpose() * error
+  weights=np.mat(weights).reshape((3,1))
   return weights
+
+def stocGradAscent0(dataMatrix, classLabels,numIter=150):
+  m, n = np.shape(dataMatrix)
+  dataMatrix = np.array(dataMatrix)
+  weights = np.ones(n)
+  for k in range(numIter):
+    dataIndex=list(range(m))
+    for i in range(m):
+      alpha = 4 / (1.0+ i + k) + 0.01  #learning rate
+      randIndex=int(random.uniform(0,len(dataIndex)))
+      h = sigmoid(sum(dataMatrix[randIndex] * weights))
+      error = classLabels[randIndex] - h
+      weights = weights + alpha * error * dataMatrix[randIndex]
+      del(dataIndex[randIndex])
+  return weights
+
+def plotBestFit(weights, dataMat, labelMat):
+  dataArr = np.array(dataMat)
+  n = np.shape(dataArr)[0]
+  xcord1 = []
+  xcord2 = []
+  ycord1 = []
+  ycord2 = []
+  for i in range(n):
+    if int(labelMat[i]) == 1:
+      xcord1.append(dataArr[i, 1])
+      ycord1.append(dataArr[i, 2])
+    else:
+      xcord2.append(dataArr[i, 1])
+      ycord2.append(dataArr[i, 2])
+  fig = plt.figure()
+  ax = fig.add_subplot(111)
+  ax.scatter(xcord1, ycord1, s=30, c='red', marker='s')
+  ax.scatter(xcord2, ycord2, s=30, c='green')
+  x = np.arange(-3.0, 3.0, 0.1)
+  y = ((-weights[0] - weights[1] * x) / weights[2]).transpose()#transpose to keep dimension ocnsistancy
+  print(x.shape,y.shape)
+  ax.plot(x, y)
+  plt.xlabel('X1')
+  plt.ylabel('X2')
+  plt.show()
 
 if __name__ == '__main__':
   dataMat, labelMat = loadDataset('testSet.txt')
-  weights = gradAscent(dataMat, labelMat)
+  #weights = gradAscent(dataMat, labelMat)
+  weights = stocGradAscent0(dataMat, labelMat,50)
   print(weights)
+  plotBestFit(weights, dataMat, labelMat)
