2cffbb695216f873307070ef5185d104abda97c8
AndyLiming <mingli6250@163.com>
2019-01-06 21:43:42 +0800

    update ch06 SVM

--- SVM/svmMLiA.py
+++ SVM/svmMLiA.py
@@ -73,6 +73,43 @@ def smoSimple(dataMatIn, classLabels, C, toler, maxIter):
   print(m,n)
   return b, alphas
 
+class optStruct:
+  def __init__(self, dataMatIn, classLabels, C, toler):
+    self.X = dataMatIn
+    self.labelMat = classLabels
+    self.C = C
+    self.tol = toler
+    self.m = np.shape(dataMatIn)[0]
+    self.alphas = np.mat(np.zeros((self.m, 1)))
+    self.b = 0
+    self.eCache = np.mat(np.zeros((self.m, 2)))
+
+def calcEk(oS, k):
+  fXk = float(np.multiply(oS.alphas, oS.labelMat).T * oS.X * oS.X[k,:].T) + oS.b
+  Ek = fXk - float(oS.labelMat[k])
+  return Ek
+
+def selectJ(i, oS, Ei):
+  maxK = -1; maxDeltaE = 0; Ej = 0
+  oS.eCache[i] = [1,Ei]
+  validEcacheList = nonzero(oS.eCache[:,0].A)[0]
+  if (len(validEcacheList)) > 1:
+    for k in validEcacheList:
+      if k == i: continue
+      Ek = calcEk(oS, k)
+      deltaE = abs(Ei - Ek)
+      if (deltaE > maxDeltaE):
+      axK = k; maxDeltaE = deltaE; Ej = Ek
+    return maxK, Ej
+  else:
+    j = selectJrand(i, oS.m)
+    Ej = calcEk(oS, j)
+  return j, Ej
+
+def updateEk(oS, k):
+  Ek = calcEk(oS, k)
+  os.eCache[k]=[1,Ek]
+
 if __name__ == '__main__':
   dataMat, labelMat = loadDataset('testSet.txt')
   print(labelMat)
