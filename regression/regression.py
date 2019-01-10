0808afd967a895f38cd82f3956ab9fe5acfd47a4
AndyLiming <mingli6250@163.com>
2019-01-10 21:44:45 +0800

    update ch08 regression

--- regression/regression.py
+++ regression/regression.py
@@ -28,6 +28,28 @@ def standRegres(xArr, yArr):
   ws = xTx.I * (xMat.T * yMat)
   return ws
 
+def lwlr(testPoint, xArr, yArr, k=1.0):
+  xMat = np.mat(xArr)
+  yMat = np.mat(yArr).T
+  m = np.shape(xMat)[0]
+  weights = np.mat(np.eye((m)))
+  for j in range(m):
+    diffMat = testPoint - xMat[j,:]
+    weights[j, j] = np.exp(diffMat * diffMat.T / (-2.0 * k ** 2))
+  xTx = xMat.T * (weights * xMat)
+  if np.linalg.det(xTx) == 0.0:
+    print("this matrix is singular, cannot do inverse")
+    return
+  ws = xTx.I * (xMat.T * (weights*yMat))
+  return testPoint * ws
+
+def lwlrTest(testArr, xArr, yArr, k=1.0):
+  m = np.shape(testArr)[0]
+  yHat = np.zeros(m)
+  for i in range(m):
+    yHat[i] lwlr(testArr[i], xArr, yArr, k)
+  return yHat
+
 if __name__ == '__main__':
   xArr, yArr = loadDataset('ex0.txt')
   ws = standRegres(xArr, yArr)
