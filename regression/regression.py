fc563178b7574006066cae1c8c963695860322a8
AndyLiming <mingli6250@163.com>
2019-01-11 16:18:35 +0800

    update ch08 regression

--- regression/regression.py
+++ regression/regression.py
@@ -3,6 +3,9 @@ import math
 import random
 import os
 import matplotlib.pyplot as plt
+import json
+from time import sleep
+import urllib3
 
 def loadDataset(fileName):
   numFeat = len(open(fileName).readline().split('\t'))
@@ -50,8 +53,69 @@ def lwlrTest(testArr, xArr, yArr, k=1.0):
     yHat[i] = lwlr(testArr[i], xArr, yArr, k)
   return yHat
 
+def ridgeRegres(xMat, yMat, lam=0.2):
+  xTx = xMat.T * xMat
+  denom = xTx + np.eye(np.shape(xMat)[1]) * lam
+  if np.linalg.det(denom) == 0.0:
+    print("this matrix is singular, cannot do inverse")
+    return
+  ws = denom.I * (xMat.T * yMat)
+  return ws
+
+def ridgeTest(xArr, yArr):
+  xMat = np.mat(xArr)
+  yMat = np.mat(yArr).T
+  yMean = np.mean(yMat, 0)
+  yMat = yMat - yMean
+  xMeans = np.mean(xMat, 0)
+  xVar = np.var(xMat, 0)
+  xMat = (xMat - xMeans) / xVar
+  numTestPts = 30
+  wMat = np.zeros((numTestPts, np.shape(xMat)[1]))
+  for i in range(numTestPts):
+    ws = ridgeRegres(xMat, yMat, math.exp(i - 10))
+    wMat[i,:] = ws.T
+  return wMat
+
+def regularize(xMat):
+  inMat = xMat.copy()
+  inMeans = np.mean(inMat,0)
+  inVar = np.var(inMat,0)
+  inMat = (inMat - inMeans)/inVar
+  return inMat
+
+def stageWise(xArr, yArr, eps=1.0, numIt=100):
+  xMat = np.mat(xArr)
+  yMat = np.mat(yArr).T
+  yMean = np.mean(yMat, 0)
+  yMat = yMat - yMean
+  xMat = regularize(xMat)
+  m, n = np.shape(xMat)
+  returnMat = np.zeros((numIt, n))
+  ws = np.zeros((n, 1))
+  wsTest = ws.copy()
+  wsMax = ws.copy()
+  for i in range(numIt):
+    print(ws.T)
+    lowestError = np.inf
+    for j in range(n):
+      for sign in [-1, 1]:
+        wsTest = ws.copy()
+        wsTest[j] += eps * sign
+        yTest = xMat * wsTest
+        rssE = rssError(yMat.A, yTest.A)
+        if rssE < lowestError:
+          lowestError = rssE
+          wsMax = wsTest
+    ws = wsMax.copy()
+    returnMat[i,:] = ws.T
+  return returnMat
+
+def rssError(yArr, yHatArr):
+  return ((yArr-yHatArr)**2).sum() #SSD
+
 if __name__ == '__main__':
-  xArr, yArr = loadDataset('ex0.txt')
+  # xArr, yArr = loadDataset('ex0.txt')
   # ws = standRegres(xArr, yArr)
   # print(ws)
   # xMat = np.mat(xArr)
@@ -64,13 +128,39 @@ if __name__ == '__main__':
   # yHat = xCopy * ws
   # ax.plot(xCopy[:, 1], yHat)
   # plt.show()
-  yHat = lwlrTest(xArr, xArr, yArr, 0.03)
-  xMat = np.mat(xArr)
-  yMat = np.mat(yArr)
-  srtInd = xMat[:, 1].argsort(0)
-  xSort = xMat[srtInd][:, 0,:]
+
+  # yHat = lwlrTest(xArr, xArr, yArr, 0.03)
+  # xMat = np.mat(xArr)
+  # yMat = np.mat(yArr)
+  # srtInd = xMat[:, 1].argsort(0)
+  # xSort = xMat[srtInd][:, 0,:]
+  # fig = plt.figure()
+  # ax = fig.add_subplot(111)
+  # ax.scatter(xMat[:, 1].flatten().A[0], yMat.T[:, 0].flatten().A[0],s=2,c='red')
+  # ax.plot(xSort[:, 1], yHat[srtInd])
+  # plt.show()
+
+  xArr, yArr = loadDataset('abalone.txt')
   fig = plt.figure()
   ax = fig.add_subplot(111)
-  ax.scatter(xMat[:, 1].flatten().A[0], yMat.T[:, 0].flatten().A[0],s=2,c='red')
-  ax.plot(xSort[:, 1], yHat[srtInd])
+  # ridgeWeights = ridgeTest(abX, abY)
+  # fig = plt.figure()
+  # ax = fig.add_subplot(111)
+  # ax.plot(ridgeWeights)
+  # plt.show()
+
+  # least square #
+  xMat = np.mat(xArr)
+  yMat = np.mat(yArr).T
+  yMean = np.mean(yMat, 0)
+  yMat = yMat - yMean
+  xMat = regularize(xMat)
+  weightsLs = standRegres(xMat, yMat.T).T
+  #-------------------#
+
+  weightsSw = stageWise(xArr, yArr, 0.01, 200)
+  
+  print("LS: ", weightsLs)
+  print("stageWise: ", weightsSw[-1])
+  ax.plot(weightsSw)
   plt.show()
