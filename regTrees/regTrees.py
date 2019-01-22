2be1176e35035b367c04ad21cdaf22b8fe3f01bc
AndyLiming <mingli6250@163.com>
2019-01-22 15:31:59 +0800

    update ch09 reg trees

--- regTrees/regTrees.py
+++ regTrees/regTrees.py
@@ -6,3 +6,15 @@ import matplotlib.pyplot as plt
 import json
 from time import sleep
 import urllib
+
+class treeNode():
+  def __init__(self, feat, val, right, left):
+    featureToSplitOn = feat
+    valueOfSplit = val
+    rightBranch = right
+    leftBranch = left
+
+
+
+
+if __name__=='__main__':
